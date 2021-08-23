aef00837fe614b95a10aa02086891581a75f53f5
Protasioland <natalia.protasio.o@gmail.com>
2021-08-23 11:54:15 -0300

    Fundamentos Web

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/*
+por Tag: getElementByTagName()
+por Id: getElementById()
+por nome: getElementByName()
+por Classe: getElementsByClassName()
+por Seletor: quertSelector()
+*/
+
+let nome = window.document.getElementById("nome")
+let email = document.querySelector("#email")
+let assunto = document.querySelector("#assunto")
+
+nome.style.width = "100%"
+email.style.width = "100%" 
+
